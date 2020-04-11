312039a7a0f8b5a4705910e9460f59970dfdac55
Suresh923 <52421700+Suresh923@users.noreply.github.com>
2020-04-11 13:56:56 +0530

    added

---
+++ authorizeEmployer.php
@@ -0,0 +1,13 @@
+<?php
+
+
+$eid="";
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+if(isset($_SESSION["eid"])){
+    $eid=$_SESSION["eid"];
+    
+}else{
+    header('Location:index.php');
+}
